d52439f2aa7c65ac871636d63f00e9157ffb0e53
Ludvig Lindblom <ludvig.lindblom@gmail.com>
2024-04-23 13:30:58 +0200

    Copy changes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,18 +4,16 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1>About View Transitions API</h1>
 
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
+		The View Transitions API provides a mechanism for easily creating animated transitions between
+		different DOM states while also updating the DOM contents in a single step.
 	</p>
 
-	<pre>npm create svelte@latest</pre>
-
 	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
+		<a href="https://developer.mozilla.org/en-US/docs/Web/API/View_Transitions_API"
+			>https://developer.mozilla.org/en-US/docs/Web/API/View_Transitions_API</a
+		>
 	</p>
 </div>
